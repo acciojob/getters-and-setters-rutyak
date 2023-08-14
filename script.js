52707dae34373b50c9c7b2db587f2bd0386274fb
rutyak <132219103+rutyak@users.noreply.github.com>
2023-08-14 13:52:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,41 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  
-  get name() {
-    return this._name;
-  }
-  
-  set name(value) {
-    this._name = value;
-  }
-  
-  get age() {
-    return this._age;
-  }
-  
-  set age(value) {
-    this._age = value;
-  }
-  
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+class Person{
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
+
+	get name( ){
+		return this._name 
+	}
+
+	set age(newAge){
+		this._age = newAge;
+	}
 }
 
-class Student extends Person {
-  study() {
-    console.log(`${this.name} is studying.`);
-  }
+class Student extends Person{
+	study(){
+	    console.log(`${this._name} is studying`);
+	}
 }
 
-class Teacher extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-  
-  teach() {
-    console.log(`${this.name} is teaching.`);
-  }
-  
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
+class Teacher extends Person{
+	teach(){
+		console.log(`${this._name} is teaching`)
+	}
 }
+
+const stu = new Student("Jon", 16);
+console.log(stu.name);
+stu.study();
+
+const teach = new Teacher("Msr. Kumar", 29);
+console.log(teach.name);
+teach.teach();
+
+
+
+
 
 // Do not change the code below this line
 window.Person = Person;
